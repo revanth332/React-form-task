db73f7e0ef1d2b26d071c8f3d66f9138be9add18
revanth332 <revathipathilanka347@gmail.com>
2024-08-27 12:28:10 +0530

    close button added after file selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { z } from "zod";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -10,7 +10,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
 // Define the schema for validation using Zod
 const schema = z.object({
@@ -21,28 +21,35 @@ const schema = z.object({
     .max(100, "Must be less than 100"),
   email: z.string().email("Invalid email address"),
   password: z.string().refine((pwd) => {
-    const pattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
-    return pattern.test(pwd)
-  },"Password didnt meet the criteria"),
-  dob: z.string().refine((val) => !isNaN(Date.parse(val)) && (Date.parse(val) < Date.now()), "Invalid date"),
+    const pattern =
+      /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
+    return pattern.test(pwd);
+  }, "Password didnt meet the criteria"),
+  dob: z
+    .string()
+    .refine(
+      (val) => !isNaN(Date.parse(val)) && Date.parse(val) < Date.now(),
+      "Invalid date"
+    ),
   color: z.string(),
   course: z.string().array(),
   gender: z.enum(["Male", "Female"]),
   file: z.any().refine((file) => file?.size > 0, "File is required"),
   range: z.string(),
   url: z.string().url("Invalid URL"),
-  time:z.string(),
-  dateTime:z.string(),
-  timeZone:z.string()
+  time: z.string(),
+  dateTime: z.string(),
+  timeZone: z.string(),
 });
 
 type FormData = z.infer<typeof schema>;
 
 const App: React.FC = () => {
-  const fileRef = useRef<HTMLInputElement>(null)
+  const fileRef = useRef<HTMLInputElement>(null);
+  const closeRef = useRef<HTMLButtonElement>(null);
 
-  const [timeZone, setTimeZone] = useState('Asia/Kolkata');
-  const [fileType,setFileType] = useState("")
+  const [timeZone, setTimeZone] = useState("Asia/Kolkata");
+  const [fileType, setFileType] = useState("");
   const [formData, setFormData] = useState<FormData>({
     name: "",
     age: 0,
@@ -55,9 +62,9 @@ const App: React.FC = () => {
     file: null,
     range: "50",
     url: "",
-    time:"",
-    dateTime:"",
-    timeZone:""
+    time: "",
+    dateTime: "",
+    timeZone: "",
   });
 
   const [errors, setErrors] = useState<Partial<Record<keyof FormData, string>>>(
@@ -65,27 +72,28 @@ const App: React.FC = () => {
   );
 
   const timeZones = [
-    { value: 'Asia/Kolkata', label: 'India Standard Time (IST)' },
-    { value: 'America/New_York', label: 'New York (EST)' },
-    { value: 'Europe/London', label: 'London (GMT)' },
-    { value: 'Asia/Tokyo', label: 'Tokyo (JST)' },
-    { value: 'Australia/Sydney', label: 'Sydney (AEDT)' },
+    { value: "Asia/Kolkata", label: "India Standard Time (IST)" },
+    { value: "America/New_York", label: "New York (EST)" },
+    { value: "Europe/London", label: "London (GMT)" },
+    { value: "Asia/Tokyo", label: "Tokyo (JST)" },
+    { value: "Australia/Sydney", label: "Sydney (AEDT)" },
   ];
 
   const fileTypes = [
-    { label: 'Images', value: 'image/*' },
-    { label: 'PDF', value: 'application/pdf' },
-    { label: 'Text Files', value: 'text/plain' },
+    { label: "Images", value: "image/*" },
+    { label: "PDF", value: "application/pdf" },
+    { label: "Text Files", value: "text/plain" },
   ];
-
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value, type, checked, files } = e.target;
     if (type === "checkbox") {
-      setFormData((prevData :FormData ) => {
-        const updatedArray : string[] = checked
-          ? [...(prevData[name as keyof FormData]), value] 
-          : (prevData[name as keyof FormData] as string[]).filter((item) => item !== value); 
+      setFormData((prevData: FormData) => {
+        const updatedArray: string[] = checked
+          ? [...prevData[name as keyof FormData], value]
+          : (prevData[name as keyof FormData] as string[]).filter(
+              (item) => item !== value
+            );
 
         return {
           ...prevData,
@@ -93,13 +101,14 @@ const App: React.FC = () => {
         };
       });
     } else {
-      if(type === "file"){
-        console.log(files)
+      if (type === "file") {
+        console.log(files);
       }
       // Handle other input types if necessary
       setFormData({
         ...formData,
-        [name]: type === "file" ? files?.[0] : type === "number" ? +value : value,
+        [name]:
+          type === "file" ? files?.[0] : type === "number" ? +value : value,
       });
     }
   };
@@ -107,21 +116,22 @@ const App: React.FC = () => {
   useEffect(() => {
     if (fileType) {
       fileRef.current?.click();
+      closeRef.current?.click();
     }
   }, [fileType]);
 
-  const handleFileType = (type:string) => {
+  const handleFileType = (type: string) => {
     setFileType(() => {
       return type;
-  });
-  }
+    });
+  };
 
   const handleTimeZoneChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTimeZone(e.target.value);
   };
 
   const handleReset = () => {
-    console.log("reset")
+    console.log("reset");
     setFormData({
       name: "",
       age: 0,
@@ -134,21 +144,21 @@ const App: React.FC = () => {
       file: null,
       range: "0",
       url: "",
-      time:"",
-      dateTime:"",
-      timeZone:""
-    })
-    setErrors({})
-  }
+      time: "",
+      dateTime: "",
+      timeZone: "",
+    });
+    setErrors({});
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("no safe",formData)
+    console.log("no safe", formData);
     const validationResult = schema.safeParse(formData);
 
     if (validationResult.success) {
       console.log("Form data is valid:", formData);
-      console.log("safe",formData)
+      console.log("safe", formData);
       setErrors({});
     } else {
       const formattedErrors: Partial<Record<keyof FormData, string>> = {};
@@ -158,17 +168,16 @@ const App: React.FC = () => {
       });
       setErrors(formattedErrors);
     }
-
   };
 
   const getConvertedDateTime = () => {
-    if (!formData.dateTime) return '';
+    if (!formData.dateTime) return "";
 
     const date = new Date(formData.dateTime);
-    const dateStr = date.toLocaleString('en-US', { timeZone });
+    const dateStr = date.toLocaleString("en-US", { timeZone });
     return dateStr;
   };
-  
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -256,45 +265,45 @@ const App: React.FC = () => {
       </div>
 
       <div className="flex flex-col">
-      <label className="text-gray-700">Courses:</label>
-      <div className="flex items-center space-x-4 mt-1">
-        <label className="text-gray-700 mr-2">
-        <input
-          type="checkbox"
-          name="course"
-          checked={formData.course.includes("React")}
-          value="React"
-          onChange={handleChange}
-          className="focus:outline-none focus:ring focus:border-blue-300"
-        />
-        <span className="ml-2">React</span>
-        </label>
-        <label className="text-gray-700 mr-2">
-        <input
-          type="checkbox"
-          name="course"
-          value="Node"
-          checked={formData.course.includes("Node")}
-          onChange={handleChange}
-          className="focus:outline-none focus:ring focus:border-blue-300"
-        />
-        <span className="ml-2">Node</span>
-        </label>
-        <label className="text-gray-700 mr-2">
-        <input
-          type="checkbox"
-          name="course"
-          value="Python"
-          checked={formData.course.includes("Python")}
-          onChange={handleChange}
-          className="focus:outline-none focus:ring focus:border-blue-300"
-        />
-        <span className="ml-2">Python</span>
-        </label>
-        {errors.course && (
-          <p className="text-red-600 text-sm mt-1 ml-2">{errors.course}</p>
-        )}
-      </div>
+        <label className="text-gray-700">Courses:</label>
+        <div className="flex items-center space-x-4 mt-1">
+          <label className="text-gray-700 mr-2">
+            <input
+              type="checkbox"
+              name="course"
+              checked={formData.course.includes("React")}
+              value="React"
+              onChange={handleChange}
+              className="focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <span className="ml-2">React</span>
+          </label>
+          <label className="text-gray-700 mr-2">
+            <input
+              type="checkbox"
+              name="course"
+              value="Node"
+              checked={formData.course.includes("Node")}
+              onChange={handleChange}
+              className="focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <span className="ml-2">Node</span>
+          </label>
+          <label className="text-gray-700 mr-2">
+            <input
+              type="checkbox"
+              name="course"
+              value="Python"
+              checked={formData.course.includes("Python")}
+              onChange={handleChange}
+              className="focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <span className="ml-2">Python</span>
+          </label>
+          {errors.course && (
+            <p className="text-red-600 text-sm mt-1 ml-2">{errors.course}</p>
+          )}
+        </div>
       </div>
 
       <div className="flex flex-col">
@@ -331,40 +340,62 @@ const App: React.FC = () => {
       <div className="flex flex-col">
         <label className="text-gray-700">File:</label>
         <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">chose file</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-1 items-center gap-4">
-          <Button variant="outline" onClick={() => handleFileType(fileTypes[0].value)}>Image</Button>
-          <Button variant="outline" onClick={() => handleFileType(fileTypes[1].value)}>Pdf</Button>
-          <Button variant="outline" onClick={() => handleFileType(fileTypes[2].value)}>Text</Button>
-        </div>
-        </div>
-      </DialogContent>
-    </Dialog>
-        
+          <DialogTrigger asChild>
+            <Button variant="outline">chose file</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-1 items-center gap-4">
+                <Button
+                  variant="outline"
+                  onClick={() => handleFileType(fileTypes[0].value)}
+                >
+                  Image
+                </Button>
+                <Button
+                  variant="outline"
+                  onClick={() => handleFileType(fileTypes[1].value)}
+                >
+                  Pdf
+                </Button>
+                <Button
+                  variant="outline"
+                  onClick={() => handleFileType(fileTypes[2].value)}
+                >
+                  Text
+                </Button>
+              </div>
+            </div>
+            <DialogFooter className="sm:justify-start">
+              <DialogClose asChild>
+                <Button ref={closeRef} type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
         {errors.file && (
           <p className="text-red-600 text-sm mt-1">{errors.file}</p>
         )}
         {formData.file?.name}
       </div>
       <input
-          type="file"
-          name="file"
-          accept={fileType}
-          hidden={true}
-          ref={fileRef}
-          onChange={handleChange}
-          className="focus:outline-none focus:ring focus:border-blue-300"
-        />
+        type="file"
+        name="file"
+        accept={fileType}
+        hidden={true}
+        ref={fileRef}
+        onChange={handleChange}
+        className="focus:outline-none focus:ring focus:border-blue-300"
+      />
 
       <div className="flex flex-col">
         <label className="text-gray-700">Range:</label>
@@ -380,7 +411,7 @@ const App: React.FC = () => {
         {errors.range && (
           <p className="text-red-600 text-sm mt-1">{errors.range}</p>
         )}
-        <p>progress : {formData.range+"%"}</p>
+        <p>progress : {formData.range + "%"}</p>
       </div>
 
       <div className="flex flex-col">
@@ -397,37 +428,37 @@ const App: React.FC = () => {
         )}
       </div>
 
-
       <div className="flex flex-col">
-      <label className="text-gray-700"> Date Time</label>
-          <input
-            type="datetime-local"
-            name="dateTime"
-            value={formData.dateTime}
-            onChange={handleChange}
-            className="border rounded-md px-2 py-1 mt-1 focus:outline-none focus:ring focus:border-blue-300"
-          />
-          {errors.dateTime && <p className="text-red-600 text-sm mt-1">{errors.dateTime}</p>}
+        <label className="text-gray-700"> Date Time</label>
+        <input
+          type="datetime-local"
+          name="dateTime"
+          value={formData.dateTime}
+          onChange={handleChange}
+          className="border rounded-md px-2 py-1 mt-1 focus:outline-none focus:ring focus:border-blue-300"
+        />
+        {errors.dateTime && (
+          <p className="text-red-600 text-sm mt-1">{errors.dateTime}</p>
+        )}
       </div>
 
       <div className="flex flex-col">
-      <label className="text-gray-700">Select Time Zone</label>
-          <select
-            name="timeZone"
-            value={timeZone}
-            onChange={handleTimeZoneChange}
-            className="border rounded-md px-2 py-1 mt-1 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            {timeZones.map((zone) => (
-              <option key={zone.value} value={zone.value}>
-                {zone.label}
-              </option>
-            ))}
-          </select>
-          <br />
-          {getConvertedDateTime()}
+        <label className="text-gray-700">Select Time Zone</label>
+        <select
+          name="timeZone"
+          value={timeZone}
+          onChange={handleTimeZoneChange}
+          className="border rounded-md px-2 py-1 mt-1 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          {timeZones.map((zone) => (
+            <option key={zone.value} value={zone.value}>
+              {zone.label}
+            </option>
+          ))}
+        </select>
+        <br />
+        {getConvertedDateTime()}
       </div>
-
 
       <div className="flex flex-col">
         <input
@@ -447,7 +478,6 @@ const App: React.FC = () => {
       >
         Submit
       </button>
-      
     </form>
   );
 };
